14f1ceafb289ac8d023838312fdea02fec27aa7d
theskinnycoder <18131a0505@gvpce.ac.in>
2024-06-05 05:01:13 +0530

    move from tsup to vite as build tool

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,42 @@
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 export default defineConfig({
-	plugins: [react()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, "src/index.ts"),
+			name: "rangu",
+			formats: ["es"],
+			fileName: "index",
+		},
+		sourcemap: true,
+		rollupOptions: {
+			external: ["react", "react-dom", "react/jsx-runtime"],
+			output: {
+				preserveModules: false,
+			},
+		},
+	},
+	plugins: [
+		react(),
+		libInjectCss(),
+		dts({ copyDtsFiles: true, rollupTypes: true }),
+	],
+	css: {
+		postcss: {
+			plugins: [tailwindcss("./tailwind.config.ts"), autoprefixer],
+			from: "src/index.css",
+			to: "dist/index.css",
 		},
 	},
 });
